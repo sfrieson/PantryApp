a08973b7f2d075cc4d32afc0963d885be52bdb4a
Steven Frieson <sfrieson@gmail.com>
2016-02-05 18:02:17 -0500

    Adds Working nutrition DB and notes on how to create it

---
+++ raw_data/format_db.js
@@ -0,0 +1,40 @@
+var fs = require('fs');
+var file;
+
+// // FOOD_DES.txt
+// file = fs.readFileSync("/Users/sfrieson/code/wdi/PantryApp/raw_data/sr28asc-master/FOOD_DES.txt", 'utf8');
+// file = file.replace(/\^{4}/g, "^0^0^0^");
+// file = file.replace(/\^{3}/g, "^0^0^");
+// file = file.replace(/\^{2}/g, "^0^");
+// file = file.replace(/\^{1}$/gm, "^0");
+// result = /\^{1}$/gm.test(file);
+// fs.writeFile("/Users/sfrieson/code/wdi/PantryApp/raw_data/sr28asc/FOOD_DES.txt", file);
+
+
+// // NUT_DATA.txt
+// file = fs.readFileSync("/Users/sfrieson/code/wdi/PantryApp/raw_data/sr28asc-master/NUT_DATA.txt", 'utf8');
+// file = file.replace(/\^{7}/g, "^0^0^0^0^0^0^");
+// file = file.replace(/\^{6}/g, "^0^0^0^0^0^");
+// file = file.replace(/\^{5}/g, "^0^0^0^0^");
+// file = file.replace(/\^{4}/g, "^0^0^0^");
+// file = file.replace(/\^{3}/g, "^0^0^");
+// file = file.replace(/\^{2}/g, "^0^");
+// //change to micrograms
+// fs.writeFile("/Users/sfrieson/code/wdi/PantryApp/raw_data/sr28asc/NUT_DATA.txt", file);
+
+
+// FD_GROUP.txt
+// UNNECESSARY
+
+// // WEIGHT.txt
+// file = fs.readFileSync("/Users/sfrieson/code/wdi/PantryApp/raw_data/sr28asc-master/WEIGHT.txt", 'utf8');
+// file = file.replace(/\^{2}/g, "^0^");
+// file = file.replace(/\^{1}$/gm, "^0");
+// fs.writeFile("/Users/sfrieson/code/wdi/PantryApp/raw_data/sr28asc/WEIGHT.txt", file);
+
+//
+
+
+
+
+// console.log(result);
